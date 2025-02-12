7cb447a1d74bb69cd48d60adf91420bdf52daed0
Nadia Batchelor <nadabdesigns@gmail.com>
2025-02-11 23:36:55 -0500

    36:08

--- arena.js
+++ arena.js
@@ -28,6 +28,105 @@ let placeChannelInfo = (data) => {
 	// channelLink.href = `https://www.are.na/channel/${channelSlug}`
 }
 
+// Then our big function for specific-block-type rendering:
+let renderBlock = (block) => {
+	// To start, a shared `ul` where we’ll insert all our blocks
+	let channelBlocks = document.querySelector('#channel-blocks')
+
+	// Links!
+	if (block.class == 'Link') {
+		let linkItem =
+			`
+			<li>
+				<p><em>Link</em></p>
+				<picture>
+					<source media="(max-width: 428px)" srcset="${block.image.thumb.url}">
+					<source media="(max-width: 640px)" srcset="${block.image.large.url}">
+					<img src="${block.image.original.url}">
+				</picture>
+				<h3>${block.title}</h3>
+				${block.description_html}
+				<p><a href="${block.source.url}">See the original ↗</a></p>
+			</li>
+			`
+		channelBlocks.insertAdjacentHTML('beforeend', linkItem)
+	}
+
+	// Images!
+	else if (block.class == 'Image') {
+		// …up to you!
+	}
+
+	// Text!
+	else if (block.class == 'Text') {
+		// …up to you!
+	}
+
+	// Uploaded (not linked) media…
+	else if (block.class == 'Attachment') {
+		let attachment = block.attachment.content_type // Save us some repetition
+
+		// Uploaded videos!
+		if (attachment.includes('video')) {
+			// …still up to you, but we’ll give you the `video` element:
+			let videoItem =
+				`
+				<li>
+					<p><em>Video</em></p>
+					<video controls src="${block.attachment.url}"></video>
+				</li>
+				`
+			channelBlocks.insertAdjacentHTML('beforeend', videoItem)
+			// More on video, like the `autoplay` attribute:
+			// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video
+		}
+
+		// Uploaded PDFs!
+		else if (attachment.includes('pdf')) {
+			// …up to you!
+		}
+
+		// Uploaded audio!
+		else if (attachment.includes('audio')) {
+			// …still up to you, but here’s an `audio` element:
+			let audioItem =
+				`
+				<li>
+					<p><em>Audio</em></p>
+					<audio controls src="${block.attachment.url}"></video>
+				</li>
+				`
+			channelBlocks.insertAdjacentHTML('beforeend', audioItem)
+			// More on audio: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio
+		}
+	}
+
+	// Linked media…
+	else if (block.class == 'Media') {
+		let embed = block.embed.type
+
+		// Linked video!
+		if (embed.includes('video')) {
+			// …still up to you, but here’s an example `iframe` element:
+			let linkedVideoItem =
+				`
+				<li>
+					<p><em>Linked Video</em></p>
+					${block.embed.html}
+				</li>
+				`
+			channelBlocks.insertAdjacentHTML('beforeend', linkedVideoItem)
+			// More on iframe: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe
+		}
+
+		// Linked audio!
+		else if (embed.includes('rich')) {
+			// …up to you!
+		}
+	}
+}
+
+
 
 
 
@@ -37,21 +136,20 @@ fetch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, { cache: 'no-stor
 	.then((response) => response.json()) // Return it as JSON data
 	.then((data) => { // Do stuff with the data
 		console.log(data) // Always good to check your response!
-		
 		placeChannelInfo(data) // Pass the data to the first function
 		
 
 
 		// Loop through the `contents` array (list), backwards. Are.na returns them in reverse!
 		data.contents.reverse().forEach((block) => {
-			console.log(block) // The data for a single block
+			// console.log(block) // The data for a single block
 			renderBlock(block) // Pass the single block data to the render function
 		})
 
 		// Also display the owner and collaborators:
-		let channelUsers = document.querySelector('#channel-users') // Show them together
-		data.collaborators.forEach((collaborator) => renderUser(collaborator, channelUsers))
-		renderUser(data.user, channelUsers)
+		// let channelUsers = document.querySelector('#channel-users') // Show them together
+		// data.collaborators.forEach((collaborator) => renderUser(collaborator, channelUsers))
+		// renderUser(data.user, channelUsers)
 	})
 
 
