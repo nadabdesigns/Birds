5401903459e794e0bb397d64c64220b9fa9086fe
Nadia Batchelor <nadabdesigns@gmail.com>
2025-02-28 09:30:10 -0500

    Edits tutor

--- arena-2.js
+++ arena-2.js
@@ -21,12 +21,10 @@ let placeChannelInfo = (data) => {
 
 // Then our big function for specific-block-type rendering:
 let renderBlock = (block) => {
-	
 	// To start, a shared `ul` where we’ll insert all our blocks
 	let channelBlocks = document.querySelector('#channel-blocks')
 
 	// Links!
-
 	if (block.class == 'Link') {
 		let linkItem =
 			`
@@ -71,15 +69,12 @@ let renderBlock = (block) => {
 		`
 			<div class= textBlock>
 				<p><em>Link</em></p>
-				<picture class= offset>
+				<picture>
 					<source media="(max-width: 428px)" srcset="${block.image.thumb.url}">
 					<source media="(max-width: 640px)" srcset="${block.image.large.url}">
 					<img src="${block.image.original.url}">
 				</picture>
-			
-
 				<h3>${block.title}</h3>
-				
 				${block.description_html}
 				<p><a href="${block.source.url}">See the original ↗</a></p>
 			</div>
@@ -131,9 +126,7 @@ let renderBlock = (block) => {
 	}
 
 	// Linked media…
-	
 	else if (block.class == 'Media') {
-		
 		let embed = block.embed.type
 
 		// Linked video!
