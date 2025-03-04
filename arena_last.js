fcc1a08d280ecdb3fbfdf10c81cca3fb45839183
Nadia Batchelor <nadabdesigns@gmail.com>
2025-03-03 23:34:20 -0500

    popup stuff

--- arena_last.js
+++ arena_last.js
@@ -6,9 +6,13 @@ let button = document.querySelector(`#${block.title}`); // Select element by ID
 let modal = document.querySelector('#dialog'); // Select the modal
 let sunDiv = document.querySelector('#sunDiv'); // Select sunDiv
 
+
+
+
 if (button) { 
     button.onclick = () => { 
         modal.showModal(); // Open the modal
         sunDiv.style.display = "none"; // Hide sunDiv
     };
 }
+
